4deab65cc0327767cd9f5fc3d6b98f46b87dfac5
Olto <67263781+Oltopeteeh@users.noreply.github.com>
2024-10-17 13:41:58 +0300

    Add files via upload

---
+++ ForeverSmithSubModule.cs
@@ -0,0 +1,23 @@
+﻿//using SmithForever;
+//using System;
+//using System.Collections.Generic;
+//using TaleWorlds.CampaignSystem;
+////using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace SmithForever
+{
+    public class ForeverSmithSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            gameStarterObject.AddModel(new ForeverSmithModel());
+        }
+    }
+}
